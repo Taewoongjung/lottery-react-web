98b907e8795507fc9e059da847ced517367da661
Taewoongjung <aipooh8882@naver.com>
2022-01-26 17:07:47 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,26 +7,30 @@ import Web3 from 'web3';
 class App extends Component {
 
   async componentDidMount() {
-
+    await this.initWeb3();
   }
 
   initWeb3 = async () => {
-    if(window.ethereum) {
+    console.log(window.ethereum);
+    if (window.ethereum) {
+      console.log('Recent mode')
       this.web3 = new Web3(window.ethereum);
       try {
-        // Request account access if needed
-        await window.ethereum.enable();
-        // Accounts now exposed
-        // this.web3.eth.sendTransaction({/* ... */});
+          // Request account access if needed
+          await window.ethereum.enable();
+          // Acccounts now exposed
+          // this.web3.eth.sendTransaction({/* ... */});
       } catch (error) {
-        // User denied account access
-        console.log(`User denied account access error :${error}`);
+          // User denied account access...
+          console.log(`User denied account access error : ${error}`)
       }
     }
     // Legacy dapp browsers...
     else if (window.web3) {
-      window.web3 = new Web3(web3.currentProvider);
-      // this.web3.eth.sendTransaction({});
+      console.log('legacy mode')
+      this.web3 = new Web3(Web3.currentProvider);
+      // Acccounts always exposed
+      // web3.eth.sendTransaction({/* ... */});
     }
     // Non-dapp browsers...
     else {
@@ -33,7 +38,7 @@ class App extends Component {
     }
   }
   render () {
-    return(
+    return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
